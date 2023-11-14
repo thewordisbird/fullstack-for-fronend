7406d0295d7109541101ed68490660e333516b08
Justin Bird <justin@thewordisbird.dev>
2023-11-14 08:53:58 -0800

    sync to example

--- index-ws.js
+++ index-ws.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const server = require('http').createServer();
 const app = express();
+<<<<<<< Updated upstream
+=======
+const PORT = 3000;
+>>>>>>> Stashed changes
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname});
 });
 
 server.on('request', app);
+<<<<<<< Updated upstream
 server.listen(3000, function() {console.log('Server started on port 3000')});
+=======
+
+server.listen(PORT, function() {console.log('Server started on port 3000')});
+>>>>>>> Stashed changes
 
 /** Begin websockets */
 const WebSocketServer = require('ws').Server;
@@ -25,6 +34,7 @@ wss.on('connection', function connection (ws) {
   }
 
   ws.on('close', function close () {
+<<<<<<< Updated upstream
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log('A client has disconnected');
   });
@@ -35,6 +45,22 @@ wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     client.send(data);
   })
+=======
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log('A client has disconnected');
+  });
+
+  ws.on('error', function error () {
+    //
+  });
+});
+
+/** Broadcast data to all connected clients */
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+>>>>>>> Stashed changes
 };
 
 
